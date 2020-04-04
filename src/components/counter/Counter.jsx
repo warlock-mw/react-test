91e324b08af4c2f6fa5172749af3312447718594
warlock-mw <mntk.iea213@gmail.com>
2020-04-05 02:10:38 +0900

    Counter コンポーネントにて、dispatch を bindActionCreators に変更

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { increment, decrement, selectCounter } from '@/modules/counterModule';
 import CounterList from '@/components/counter/CounterList';
 
 function Counter(){
     const data     = useSelector(selectCounter);
     const dispatch = useDispatch();
+    const handle   = bindActionCreators({ increment, decrement }, dispatch);
 
+    console.log(handle);
     return (
       <div>
         <div>
-          <button onClick={() => dispatch(decrement())}>-</button>
+          <button onClick={handle.decrement}>-</button>
           <span>{data.count}</span>
-          <button onClick={() => dispatch(increment())}>+</button>
+          <button onClick={handle.increment}>+</button>
         </div>
         <hr />
         <CounterList />
